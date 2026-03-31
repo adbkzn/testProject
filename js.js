bbab04f2580e007ad778f0053535c41c92f6066d
adbkzn <adb.kzn@gmail.com>
2026-03-31 16:05:34 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,43 @@
+
+const formBtn = document.getElementById("myForm")
+formBtn.addEventListener("submit",function(e){
+    e.preventDefault();
+    console.log("sdfdsf");
+    
+
+})
+// const form = document.getElementById("myForm");
+
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault(); 
+//     console.log("Sdf");
+    
+    // const username = document.getElementById("username").value;
+    // const email = document.getElementById("email").value;
+    // if (username.length<4) {
+    //     alert("שם המשתמש חייב להיות לפחות 5 תווים");
+    // }
+    // const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // if (!emailPattern.test(email)) {
+    //     alert("כתובת אימייל לא חוקית");
+    // }
+    // alert("Data was send")
+    
+// })
+// function validateForm() {
+//     // קבלת הערכים מהטופס
+    // const username = document.getElementById("username").value;
+    // const email = document.getElementById("email").value;
+    // if (username.length<4) {
+    //     alert("שם המשתמש חייב להיות לפחות 5 תווים");
+    // }
+    // const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // if (!emailPattern.test(email)) {
+    //     alert("כתובת אימייל לא חוקית");
+    // }
+    
+
+//     // אם הכל תקין
+//     alert(username);
+//     alert(email)
+// }
